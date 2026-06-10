cded42caccb03a23bbf91df66e86308eb755568d
Sandeep <sandeeprkambli@gmail.com>
2026-06-10 07:47:22 +0530

    version-1

---
+++ p1.py
@@ -0,0 +1,5 @@
+str= 'mom'
+if str == str[::-1]:
+    print(str,"Pallindrome")
+else:
+    print(str,"not pallindrome")
---
+++ p2.py
@@ -0,0 +1,5 @@
+a=12
+if a%2==0:
+    print(a,"even")
+else:
+    print(a,"odd")
