6d4aa1727643aa8ab8e857613266ad5a4df23cce
Sandeep Kambli <sandeeprkambli@gmail.com>
2026-06-11 07:48:29 +0530

    version4

--- p3.py
+++ p3.py
@@ -3,3 +3,21 @@ if str(a)==str(a)[::-1]:
     print(a,"Pallandrome")
 else:
     print(a,"not pallandrome")
+
+
+
+
+a=list(range(-25,26))
+b=[]
+c=[]
+zero=[]
+for i in a:
+    if i > 0:
+      b.append(i)
+    elif  i < 0:
+      c.append(i)
+    else:
+      zero.append(i)
+print("Positive numbers",b)
+print("negative numbers",c)
+print(zero)
